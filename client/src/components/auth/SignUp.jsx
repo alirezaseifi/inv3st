f94c3f54de9875d8f24e317f027a18a17a47003f
Alireza Seifi <m4seifi@gmail.com>
2017-10-16 12:28:52 -0700

    inwest signup

--- client/src/components/auth/SignUp.jsx
+++ client/src/components/auth/SignUp.jsx
@@ -38,6 +38,8 @@ class SignUp extends Component {
     return (
       <div className="signup-container">
         <h1 className="hdr">Sign Up</h1>
+        <p></p>
+        <p>Inwest is currently in βeta. We're rolling out access over the next few weeks,<br/> so if you'd like to be one of the first to try, please register your interest below.</p>
         <form onSubmit={handleSubmit(this.handleFormSubmit)}>
           <Field name="first" component={renderField} className="form-field field-first" type="text" placeholder="First" />
           <Field name="last" component={renderField} className="form-field field-last" type="text" placeholder="Last" />
