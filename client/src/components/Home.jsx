dca5f9c3ac7a80e49bb98bd4f76c87bf58ed8c48
Alireza Seifi <m4seifi@gmail.com>
2017-12-15 02:53:52 -0800

    lgoo

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="container-home">
-      <iframe width="392" height="220" src="https://www.youtube.com/embed/dlXZy7CQtTk?version=3&autoplay=0&controls=0&&showinfo=0&loop=1&rel=0&modestbranding=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>  
+      <iframe width="392" height="220" src="https://www.youtube.com/embed/dlXZy7CQtTk?version=3&autoplay=0&controls=0&&showinfo=0&loop=1&rel=0&modestbranding=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>  
 
         <h1 className="hdr">every ¢ent counts</h1>
         <div className="btns">
